aa3853a5f2a7e212a31f9a88d1f56f898e84b420
Matthew Terrill <meterrill@gmail.com>
2020-05-30 07:33:41 -0700

    Revert to anonymous functions (d19)

--- 19-nyt-multiple-categories/script.js
+++ 19-nyt-multiple-categories/script.js
@@ -6,38 +6,30 @@ var sections = ['arts', 'automobiles', 'books', 'business', 'fashion', 'food', '
 var endpoint = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=';
 var apiKey = '0q2Pwnu4SiIy7UxplCJ745E0PGRohgVb';
 
-// Get the JSON from the API
-function getJSON(response) {
-  return response.ok ? response.json() : Promise.reject(response);
-}
-
-// Add a section w/ heading and the first 3 stories to the DOM
-function showSection(data) {
-  app.innerHTML = data.results.slice(0, 3).map(function (story) {
-    return (
-      '<article>' +
-      '<a href="' + story.url + '">' +
-      '<img src="' + story.multimedia[0].url + '" alt="' + story.multimedia[0].caption + '">' +
-      '<h3>' + story.title + '</h3>' +
-      '<p>' + story.abstract + '</p>' +
-      '<time datetime="' + story.published_date + '">' + new Date(story.published_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) + '</time>' +
-      '</a>' +
-      '</article>'
+// Fetch the stories from the API
+function getStories() {
+  fetch(endpoint + apiKey).then(function (response) {
+    return response.ok ? response.json() : Promise.reject(response);
+  }).then(function (data) {
+    app.innerHTML = data.results.slice(0, 3).map(function (story) {
+      return (
+        '<article>' +
+        '<a href="' + story.url + '">' +
+        '<img src="' + story.multimedia[0].url + '" alt="' + story.multimedia[0].caption + '">' +
+        '<h3>' + story.title + '</h3>' +
+        '<p>' + story.abstract + '</p>' +
+        '<time datetime="' + story.published_date + '">' + new Date(story.published_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) + '</time>' +
+        '</a>' +
+        '</article>'
       );
     }).join('');
-    
-  app.innerHTML = '<section>' + '<h2>' + 'Heading' + '</h2>' + '<div>' + app.innerHTML;
 
-  app.innerHTML += '</div>' + '</section>';
+    app.innerHTML = '<section>' + '<h2>' + 'Heading' + '</h2>' + '<div>' + app.innerHTML;
+
+    app.innerHTML += '</div>' + '</section>';
+  }).catch(function (error) {
+    app.innerHTML = '<p>We\'re experiencing some technical difficulties. Please try again later or view today\'s top stories directly on <a href="https://www.nytimes.com/">The New York Times</a> website.</p>';
+  });
 }
 
-// Add an error message to the DOM
-function showError(error) {
-  app.innerHTML = '<p>We\'re experiencing some technical difficulties. Please try again later or view today\'s top stories directly on <a href="https://www.nytimes.com/">The New York Times</a> website.</p>';
-}
-
-// Fetch the stories from the API and add them to the DOM
-fetch(endpoint + apiKey)
-  .then(getJSON)
-  .then(showSection)
-  .catch(showError);
+getStories();
