c47bb8e84abd2057b8b523ba1cd1d769ebc6a92b
Matthew Terrill <meterrill@gmail.com>
2020-05-31 11:59:05 -0700

    Add helper function to sanitize the API data

--- 21-sanitizing-the-nyt-api-data/script.js
+++ 21-sanitizing-the-nyt-api-data/script.js
@@ -6,6 +6,18 @@ var sections = ['world', 'us', 'sports', 'technology']
 var endpoint = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=';
 var apiKey = '0q2Pwnu4SiIy7UxplCJ745E0PGRohgVb';
 
+/*!
+ * Sanitize and encode all HTML in a user-submitted string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {String} str  The user-submitted string
+ * @return {String} str  The sanitized string
+ */
+var sanitizeHTML = function (str) {
+  var temp = document.createElement('div');
+  temp.textContent = str;
+  return temp.innerHTML;
+};
+
 // Fetch the stories from the API
 function getStories(section, endpoint) {
   fetch(endpoint).then(function (response) {
@@ -19,11 +31,11 @@ function getStories(section, endpoint) {
       data.results.slice(0, 3).map(function (story) {
         return (
           '<article>' +
-          '<a href="' + story.url + '">' +
-          '<img src="' + story.multimedia[0].url + '" alt="' + story.multimedia[0].caption + '">' +
-          '<h3>' + story.title + '</h3>' +
-          '<p>' + story.abstract + '</p>' +
-          '<time datetime="' + story.published_date + '">' + new Date(story.published_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) + '</time>' +
+          '<a href="' + sanitizeHTML(story.url) + '">' +
+          '<img src="' + sanitizeHTML(story.multimedia[0].url) + '" alt="' + sanitizeHTML(story.multimedia[0].caption) + '">' +
+          '<h3>' + sanitizeHTML(story.title) + '</h3>' +
+          '<p>' + sanitizeHTML(story.abstract) + '</p>' +
+          '<time datetime="' + sanitizeHTML(story.published_date) + '">' + new Date(sanitizeHTML(story.published_date)).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) + '</time>' +
           '</a>' +
           '</article>'
         );
