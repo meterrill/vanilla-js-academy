aa38ded20e21cc328dd57f9ad6218537648f24f2
Matthew Terrill <meterrill@gmail.com>
2020-06-02 09:54:37 -0700

    Shuffle monsters (d23)

--- 23-monster-shuffle/script.js
+++ 23-monster-shuffle/script.js
@@ -17,6 +17,36 @@ var monsters = [
 // Get the #app element
 var app = document.querySelector('#app');
 
+/**
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+var shuffle = function (array) {
+
+  var currentIndex = array.length;
+  var temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+
+};
+
+// Shuffle monsters
+shuffle(monsters);
+
 // Display monsters in the DOM
 app.innerHTML = `<div class="row">
                   ${monsters.map(function(monster) {
