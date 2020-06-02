d843b27620b3c108d9d791a677d5965534b72e9e
Matthew Terrill <meterrill@gmail.com>
2020-06-02 08:44:32 -0700

    Add project template (d23)

---
+++ 23-monster-shuffle/script.js
@@ -0,0 +1,17 @@
+// The monsters and socks
+var monsters = [
+  'monster1',
+  'monster2',
+  'monster3',
+  'monster4',
+  'monster5',
+  'monster6',
+  'monster7',
+  'monster8',
+  'monster9',
+  'monster10',
+  'monster11',
+  'sock'
+];
+
+// Your code goes here...
