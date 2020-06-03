7974528ff4113342541d2cc06463387ebdef8728
Matthew Terrill <meterrill@gmail.com>
2020-06-03 08:54:45 -0700

    Add alt text for better accessibility (d23)

--- 23-monster-shuffle/script.js
+++ 23-monster-shuffle/script.js
@@ -1,17 +1,53 @@
 // The monsters and socks
 var monsters = [
-  'monster1',
-  'monster2',
-  'monster3',
-  'monster4',
-  'monster5',
-  'monster6',
-  'monster7',
-  'monster8',
-  'monster9',
-  'monster10',
-  'monster11',
-  'sock'
+  { 
+    src: 'monster1', 
+    alt: 'A yellow monster with one big eye, a curly nose, and a tail.' 
+  },
+  { 
+    src: 'monster2', 
+    alt: 'A yellow monster with one big eye, two long arms, and two long legs.' 
+  },
+  { 
+    src: 'monster3', 
+    alt: 'A green monster with two big eyes, wavy arms, and sharp teeth running down its body.' 
+  },
+  { 
+    src: 'monster4', 
+    alt: 'A red monster with 2 small eyes, four arms, and downturned horns.' 
+  },
+  { 
+    src: 'monster5', 
+    alt: 'A green monster with one big eye and a glum expression.' 
+  },
+  { 
+    src: 'monster6', 
+    alt: 'A green monster with one eye and a triangular body doing a handstand.' 
+  },
+  { 
+    src: 'monster7', 
+    alt: 'A purple monster with one big eye and two tentacles for arms.' 
+  },
+  { 
+    src: 'monster8', 
+    alt: 'A purple monster with an egg-shaped body, one eye, and no arms or legs.' 
+  },
+  { 
+    src: 'monster9', 
+    alt: 'A blue, insect-like monster with two eyes, two arms, three legs, and four wings.' 
+  },
+  { 
+    src: 'monster10', 
+    alt: 'A blue, stalk-eyed monster with two small legs and no arms.' 
+  },
+  { 
+    src: 'monster11', 
+    alt: 'A black, yeti-like monster with a big smile.' 
+  },
+  { 
+    src: 'sock', 
+    alt: 'A pair of socks.' 
+  }
 ];
 
 // Get the #app element
@@ -21,7 +57,7 @@ var app = document.querySelector('#app');
  * Randomly shuffle an array
  * https://stackoverflow.com/a/2450976/1293256
  * @param  {Array} array The array to shuffle
- * @return {String}      The first item in the shuffled array
+ * @return {Array}       The shuffled array
  */
 var shuffle = function (array) {
 
@@ -51,7 +87,7 @@ shuffle(monsters);
 app.innerHTML = `<div class="row">
                   ${monsters.map(function(monster) {
                     return `<div class="grid">
-                      <img src="img/${monster}.svg" alt="${monster}">
+                      <img src="img/${monster.src}.svg" alt="${monster.alt}">
                     </div>`
                   }).join('')}
                 </div>`;
