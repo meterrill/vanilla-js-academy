9561b40abae6f62f835e8542f66b30a2435408e9
Matthew Terrill <meterrill@gmail.com>
2020-06-08 07:49:14 -0700

    Get an array of all the h2 elements (d29)

--- 29-table-of-contents/script.js
+++ 29-table-of-contents/script.js
@@ -1,2 +1,5 @@
-// Get the #table-of-contents element
+// Get the  #table-of-contents element
 var toc = document.querySelector('#table-of-contents');
+
+// Get an array of all the h2 elements
+var headingsArray = Array.prototype.slice.call(document.querySelectorAll('h2'));
