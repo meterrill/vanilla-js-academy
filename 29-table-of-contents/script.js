3ae06e95f2df01b7d0a97cc2e012c3f89eedb018
Matthew Terrill <meterrill@gmail.com>
2020-06-09 09:19:00 -0700

    Move array creation for better optimization (d29)

--- 29-table-of-contents/script.js
+++ 29-table-of-contents/script.js
@@ -1,14 +1,14 @@
 // Get the  #table-of-contents element
 var toc = document.querySelector('#table-of-contents');
 
-// Get an array of all the h2 elements
-var headings = Array.prototype.slice.call(document.querySelectorAll('h2'));
+// Get the h2 elements
+var headings = document.querySelectorAll('h2');
 
 // Only generate markup if h2 elements exist
 if (headings.length > 0) {
   // Create a list of anchor links and render to the DOM
   toc.innerHTML = `<ul>
-                  ${headings.map(function(heading) {
+                  ${Array.prototype.slice.call(headings).map(function(heading) {
                     return `<li>
                       <a href="#${heading.id}">${heading.innerText}</a>
                     </li>`
