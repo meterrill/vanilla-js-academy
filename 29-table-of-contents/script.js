8e15247156041991f70ea0fc055d14ffbe00ca93
Matthew Terrill <meterrill@gmail.com>
2020-06-09 09:06:03 -0700

    Add conditional before creating markup (d29)

--- 29-table-of-contents/script.js
+++ 29-table-of-contents/script.js
@@ -4,11 +4,14 @@ var toc = document.querySelector('#table-of-contents');
 // Get an array of all the h2 elements
 var headings = Array.prototype.slice.call(document.querySelectorAll('h2'));
 
-// Create a list of anchor links and render to the DOM
-toc.innerHTML = `<ul>
-                ${headings.map(function(heading) {
-                  return `<li>
-                    <a href="#${heading.id}">${heading.innerText}</a>
-                  </li>`
-                  }).join('')}
-                </ul>`;
+// Only generate markup if h2 elements exist
+if (headings.length > 0) {
+  // Create a list of anchor links and render to the DOM
+  toc.innerHTML = `<ul>
+                  ${headings.map(function(heading) {
+                    return `<li>
+                      <a href="#${heading.id}">${heading.innerText}</a>
+                    </li>`
+                    }).join('')}
+                  </ul>`;
+}
