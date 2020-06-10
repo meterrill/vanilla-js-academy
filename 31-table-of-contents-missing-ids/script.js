a44f1f7a017c1ea22f99bb5c9aed30a1250fb1f4
Matthew Terrill <meterrill@gmail.com>
2020-06-10 15:21:05 -0700

    Add project template (d31)

---
+++ 31-table-of-contents-missing-ids/script.js
@@ -0,0 +1,17 @@
+// Get the  #table-of-contents element
+var tableOfContents = document.querySelector('#table-of-contents');
+
+// Get the h2 elements
+var headings = document.querySelectorAll('h2');
+
+// Only generate markup if h2 elements exist
+if (headings.length > 0) {
+  // Create a list of anchor links and render to the DOM
+  tableOfContents.innerHTML = `<ul>
+                  ${Array.prototype.slice.call(headings).map(function(heading) {
+                    return `<li>
+                      <a href="#${heading.id}">${heading.innerText}</a>
+                    </li>`
+                    }).join('')}
+                  </ul>`;
+}
