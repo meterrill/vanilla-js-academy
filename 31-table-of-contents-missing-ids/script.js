67f79187899e078f406cecb48ff7cc286c77d46f
Matthew Terrill <meterrill@gmail.com>
2020-06-10 16:17:43 -0700

    Create ID from heading text (d31)

--- 31-table-of-contents-missing-ids/script.js
+++ 31-table-of-contents-missing-ids/script.js
@@ -4,14 +4,21 @@ var tableOfContents = document.querySelector('#table-of-contents');
 // Get the h2 elements
 var headings = document.querySelectorAll('h2');
 
+// Create ID from heading text
+function createID(heading) {
+	heading.id = heading.textContent.replace(/\W+/g, '-').toLowerCase();
+}
+
 // Only generate markup if h2 elements exist
 if (headings.length > 0) {
   // Create a list of anchor links and render to the DOM
   tableOfContents.innerHTML = `<ul>
                   ${Array.prototype.slice.call(headings).map(function(heading) {
 										// Check for missing IDs
-										if (!heading.id) return;
-										
+										if (!heading.id) {
+											createID(heading);
+										}
+
 										return `<li>
 											<a href="#${heading.id}">${heading.innerText}</a>
 										</li>`
