24e1ba48430dd2ddd9bdd5afe6a75b3ad543e61c
Matthew Terrill <meterrill@gmail.com>
2020-06-10 15:54:53 -0700

    Check for missing IDs (d31)

--- 31-table-of-contents-missing-ids/script.js
+++ 31-table-of-contents-missing-ids/script.js
@@ -9,9 +9,12 @@ if (headings.length > 0) {
   // Create a list of anchor links and render to the DOM
   tableOfContents.innerHTML = `<ul>
                   ${Array.prototype.slice.call(headings).map(function(heading) {
-                    return `<li>
-                      <a href="#${heading.id}">${heading.innerText}</a>
-                    </li>`
-                    }).join('')}
-                  </ul>`;
+										// Check for missing IDs
+										if (!heading.id) return;
+										
+										return `<li>
+											<a href="#${heading.id}">${heading.innerText}</a>
+										</li>`
+										}).join('')}
+									</ul>`;
 }
