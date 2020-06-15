e0af62cb32b9bb704abbfee10bd3ece668246fb4
Matthew Terrill <meterrill@gmail.com>
2020-06-15 08:50:06 -0700

    Get the current weather (d35)

--- 35-weather-app/script.js
+++ 35-weather-app/script.js
@@ -5,7 +5,14 @@ var app = document.querySelector('#app');
 fetch('https://ipapi.co/json/').then(function(response) {
   return response.ok ? response.json() : Promise.reject(response);
 }).then(function(data) {
-  console.log(data);
+  // Store the location data
+  var location = data;
+  // Get the current weather
+  return fetch(`https://api.weatherbit.io/v2.0/current?city=${location.city},${location.region_code}&key=418c15fc4a2b4812936ccb24faa2532d`);
+}).then(function(response) {
+  return response.ok ? response.json() : Promise.reject(response);
+}).then(function(weatherData) {
+  console.log(weatherData);
 }).catch(function(err) {
   console.warn('Something went wrong.' , err);
 });
