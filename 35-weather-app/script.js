1ebf36202878961cbbd02157ae2627c517dc937c
Matthew Terrill <meterrill@gmail.com>
2020-06-16 09:34:06 -0700

    Create helper function to convert temp (d35)

--- 35-weather-app/script.js
+++ 35-weather-app/script.js
@@ -4,16 +4,18 @@ var apiKey = '418c15fc4a2b4812936ccb24faa2532d';
 // Get #app element
 var app = document.querySelector('#app');
 
+// Convert temp from Celcius to Farenheit
+function tempInFarenheit(temp) {
+	return parseInt((temp * 9 / 5) + 32);
+}
+
 // Render current weather to DOM
 function renderWeather(weather) {
-	// Convert temp from Celcius to Farenheit
-	var tempInFarenheit = (weather.temp * 9 / 5) + 32;
-
 	app.innerHTML = `
 		<p>
 			<img src="https://www.weatherbit.io/static/img/icons/${weather.weather.icon}.png" alt="${weather.weather.description}">
 		</p>
-    <p>Current weather in ${weather.city_name}, ${weather.state_code} is ${tempInFarenheit} (&#8457) and ${(weather.weather.description).toLowerCase()}.</p>
+    <p>Current weather in ${weather.city_name}, ${weather.state_code} is ${tempInFarenheit(weather.temp)}&#176 and ${(weather.weather.description).toLowerCase()}.</p>
   `;  
 }
 
