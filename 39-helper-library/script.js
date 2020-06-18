e4bcb5bfd17f6d4aa46b8a924ff0dcf0a38fe535
Matthew Terrill <meterrill@gmail.com>
2020-06-18 08:40:58 -0700

    Create public method to convert NodeList to Array (d39)

--- 39-helper-library/script.js
+++ 39-helper-library/script.js
@@ -1,9 +1,14 @@
-var arrayHelper = (function() {
+var sidkik = (function() {
 
   // Public methods
   var methods = {};
 
+  // Convert a NodeList to an Array
+  methods.convertToArray = function(nodeList) {
+    return Array.prototype.slice.call(nodeList);
+  };
+
   // Return public methods
   return methods;
-  
+
 })();
