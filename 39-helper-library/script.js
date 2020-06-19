35ac2b735ca1bb0b9a57cc28c190d5934d215c2a
Matthew Terrill <meterrill@gmail.com>
2020-06-19 09:03:12 -0700

    Update comments with JSDoc (d39)

--- 39-helper-library/script.js
+++ 39-helper-library/script.js
@@ -3,29 +3,49 @@ var _ = (function() {
   // Public methods
   var methods = {};
 
-  // Convert a NodeList to an Array
+  /**
+   * Convert an array-like object into an array
+   * @param  {*}     list An array-like object (e.g., NodeList)
+   * @return {Array}      An array
+   */
   methods.toArray = function(list) {
     return Array.prototype.slice.call(list);
   };
 
-  // Get the first matching element in the DOM
+  /**
+   * Get the first element that matches a selector
+   * @param  {String} selector The selector
+   * @return {Node}            The matching element
+   */
   methods.getFirst = function(selector) {
     return document.querySelector(selector);
   };
 
-  // Get all matching elements in the DOM as an array
+  /**
+   * Get all elements that match a selector
+   * @param  {String} selector The selector
+   * @return {Array}           The matching elements
+   */
   methods.getAll = function(selector) {
     return this.toArray(document.querySelectorAll(selector));
   };
 
-  // Add a class to all elements in an array
+  /**
+   * Add a class to all elements in an array
+   * @param {Array}  array     The elements
+   * @param {String} className The class to add
+   */
   methods.addClass = function(array, className) {
     array.forEach(function(element) {
       element.classList.add(className);
     });
   };
 
-  // Remove a class from all elements in an array
+  /**
+   * Remove a class from all elements in an array
+   * @param {Array}  array     The elements
+   * @param {String} className The class to remove
+   */
   methods.removeClass = function(array, className) {
     array.forEach(function(element) {
       element.classList.remove(className);
