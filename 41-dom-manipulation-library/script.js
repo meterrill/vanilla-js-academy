810903eda27b7aef2fba52411dc032cbdbca51cd
Matthew Terrill <meterrill@gmail.com>
2020-06-20 13:36:53 -0700

    Create a constructor function (d41)

--- 41-dom-manipulation-library/script.js
+++ 41-dom-manipulation-library/script.js
@@ -1,3 +1,16 @@
 var $ = (function() {
-  
+
+  /**
+   * Create the constructor object
+   * @param {String} selector A selector
+   */
+  var Constructor = function(selector) {
+    this.elements = document.querySelectorAll(selector);
+  };
+
+  /**
+   * Return the contructor object
+   */
+  return Constructor;
+
 })();
