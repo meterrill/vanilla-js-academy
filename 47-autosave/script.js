6b4d3ca9c00bcd32221885a6dfa65db7e63c4a3a
Matthew Terrill <meterrill@gmail.com>
2020-06-26 08:42:53 -0700

    Listen for changes to form inputs (d47)

--- 47-autosave/script.js
+++ 47-autosave/script.js
@@ -5,4 +5,14 @@
   // Get #save-me form
   var form = document.querySelector('#save-me');
 
+  // Log the updated value
+  function updateValue(event) {
+    if (event.target.matches('#name')) {
+      console.log(event.target.value);
+    }
+  }
+
+  // Listen for changes to form inputs
+  form.addEventListener('input', updateValue);
+
 })();
