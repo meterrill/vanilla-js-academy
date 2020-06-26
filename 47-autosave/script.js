118cbb68301f6d67355876e3e8d8823edece5ece
Matthew Terrill <meterrill@gmail.com>
2020-06-26 09:20:27 -0700

    Get the input values from local storage (d47)

--- 47-autosave/script.js
+++ 47-autosave/script.js
@@ -4,6 +4,14 @@
 
   // Get #save-me form
   var form = document.querySelector('#save-me');
+  var name = document.querySelector('#name');
+
+  /**
+   * Get the input values from local storage
+   */
+  function getValue() {
+    name.value = localStorage.getItem('name');
+  }
 
   /**
    * Save the input value to local storage
@@ -14,6 +22,9 @@
       localStorage.setItem('name', event.target.value);
     }
   }
+
+  // Get the input values when the page loads
+  getValue();
 
   // Listen for changes to form inputs
   form.addEventListener('input', setValue);
