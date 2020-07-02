1bc2b2ee7d0525b6a0b1fa99826224b4b5711332
Matthew Terrill <meterrill@gmail.com>
2020-07-02 14:38:19 -0700

    Get data from API (d53)

--- 53-api-cache/script.js
+++ 53-api-cache/script.js
@@ -8,4 +8,24 @@
   // Get saved data from localStorage
   var saved = localStorage.getItem('scuttlebuttData');
 
+  // Store the endpoint
+  var endpoint = 'https://vanillajsacademy.com/api/pirates.json';
+
+  /**
+   * Get the data from the API
+   */
+  function getDataFromAPI() {
+    fetch(endpoint).then(function(response) {
+      return response.ok ? response.json() : Promise.reject(response);
+    }).then(function(data) {
+      console.log(data);
+    }).catch(function(error) {
+      app.innerHTML = '<p>We\'re experiencing some technical difficulties. Please try again later.</p>';
+    });
+  }
+
+  if (!saved) {
+    getDataFromAPI();
+  }  
+
 })();
