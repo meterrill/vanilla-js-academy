7c48bdb551494264540ea949b5fd594585edd54d
Matthew Terrill <meterrill@gmail.com>
2020-07-02 18:36:05 -0700

    Sanitize the data (d53)

--- 53-api-cache/script.js
+++ 53-api-cache/script.js
@@ -30,14 +30,26 @@
   };
 
   /**
+   * Sanitize and encode all HTML in a user-submitted string
+   * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+   * @param  {String} str  The user-submitted string
+   * @return {String} str  The sanitized string
+   */
+  var sanitizeHTML = function (str) {
+    var temp = document.createElement('div');
+    temp.textContent = str;
+    return temp.innerHTML;
+  };
+
+  /**
    * Display the data in the DOM
    * @param {Object} data 
    */
   function displayData(data) {
     app.innerHTML = data.articles.map(function(article) {
       return `<article>
-        <h2>${article.title}</h2>
-        <p>${article.article}</p>
+        <h2>${sanitizeHTML(article.title)}</h2>
+        <p>${sanitizeHTML(article.article)}</p>
       </article>`
     }).join('');
   }
