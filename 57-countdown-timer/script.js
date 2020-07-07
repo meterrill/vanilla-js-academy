27410ea2ee63a98de24490256e6d385587de2d8d
Matthew Terrill <meterrill@gmail.com>
2020-07-06 17:16:18 -0700

    Add restart button (d57)

--- 57-countdown-timer/script.js
+++ 57-countdown-timer/script.js
@@ -16,26 +16,40 @@ Timer.prototype.render = function() {
   this.element.innerHTML = this.template(this.data);
 }
 
-// Instantiate a new Timer component
-var app = new Timer('#app', {
-  data: {count: 60},
-  template: function(props) {
-    return `<h2>${props.count}</h2>`;
+function startTimer() {
+  // Instantiate a new Timer component
+  var app = new Timer('#app', {
+    data: {count: 60},
+    template: function(props) {
+      return `<h2>${props.count}</h2>`;
+    }
+  });
+  
+  // Run the render function and decrement every second
+  var countDown = setInterval(function() {
+  
+    // Render the Timer
+    app.render();
+    
+    // Decrement the count by 1
+    app.data.count--;
+  
+    // Clear the interval when count equals 0
+    if (app.data.count < -1) {
+      clearInterval(countDown);
+  
+      // Display a restart button
+      app.data.count = `<button data-restart>Restart</button`;
+      app.render();
+    }
+    
+  }, 1000);
+}
+
+window.addEventListener('click', function() {
+  if (event.target.matches('[data-restart]')) {
+    startTimer();
   }
 });
 
-// Run the render function and decrement every second
-var countDown = setInterval(function() {
-
-  // Render the Timer
-  app.render();
-  
-  // Decrement the count by 1
-  app.data.count--;
-  
-  // Clear the interval when count equals 0
-  if (app.data.count < 0) {
-    clearInterval(countDown);
-  }
-  
-}, 1000);
+startTimer();
