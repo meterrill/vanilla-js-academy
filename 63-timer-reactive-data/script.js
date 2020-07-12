43f8f4944dce19bb8250041714bbd18c15180878
Matthew Terrill <meterrill@gmail.com>
2020-07-12 07:54:12 -0700

    Add a proxy to the timer data (d63)

--- 63-timer-reactive-data/script.js
+++ 63-timer-reactive-data/script.js
@@ -2,13 +2,39 @@ var duration = 20;
 var timer;
 
 /**
+ * Handle proxy changes
+ * @param  {Constructor} instance The constructor instance
+ */
+function handler() {
+  return {
+		get: function (obj, prop) {
+			console.log('got it!');
+			if (['[object Object]', '[object Array]'].indexOf(Object.prototype.toString.call(obj[prop])) > -1) {
+				return new Proxy(obj[prop], handler());
+			}
+			return obj[prop];
+		},
+		set: function (obj, prop, value) {
+			console.log('set it');
+			obj[prop] = value;
+			return true;
+		},
+		deleteProperty: function (obj, prop) {
+			console.log('delete it');
+			delete obj[prop];
+			return true;
+		}
+  };
+};
+
+/**
  * State-based UI Component
  * @param {String} selector The selector for the target element
  * @param {Object} options  The component options
  */
 function Timer(selector, options) {
   this.element = document.querySelector(selector);
-  this.data = options.data;
+  this.data = new Proxy(options.data, handler());
   this.template = options.template;
 };
 
