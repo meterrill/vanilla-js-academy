102778eb4411778563a3081cb1eb411172d27e51
Matthew Terrill <meterrill@gmail.com>
2020-07-14 07:40:05 -0700

    Create a new Reef instance (d65)

--- 65-places/script.js
+++ 65-places/script.js
@@ -1,6 +1,20 @@
 var endpoint = 'https://vanillajsacademy.com/api/places.json';
 
 /**
+ * Create a new Reef instance
+ */
+var app = new Reef('#app', {
+  data: {
+    greeting: 'Hello'
+  },
+  template: function(props) {
+    return `<p>${props.greeting}</p>`;
+  }
+});
+
+app.render();
+
+/**
  * Get the data from the API
  */
 fetch(endpoint).then(function(response) {
