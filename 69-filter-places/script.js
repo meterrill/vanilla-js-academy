840e2100bfe32307583e78c5a8793ef6d6d1e211
Matthew Terrill <meterrill@gmail.com>
2020-07-21 19:40:33 -0700

    Add the filter object and functionality (d69)

--- 69-filter-places/script.js
+++ 69-filter-places/script.js
@@ -29,36 +29,6 @@ function saveToStorage(key, object) {
 }
 
 /**
- * Handle click events
- * @param {Object} event The event object
- */
-function clickHandler(event) {
-  // If the event target has an attribute of data-object
-  if (event.target.closest('[data-object]')) {
-    // Get the value of data-object
-    var object = event.target.closest('[data-object]').getAttribute('data-object');
-
-    // Get the value of data-id
-    var id = event.target.closest('[data-id]').getAttribute('data-id');
-
-    // Toggle the place's favorites value
-    app.data[object][id] = app.data[object][id] ? false : true;
-  }
-}
-
-/**
- * Handle render events
- * @param {Event} event The event object
- */
-function renderHandler(event) {
-  // Save the favorites object to localStorage
-  saveToStorage(favoritesID, app.data.favorites);
-
-  // Save the visited object to localStorage
-  saveToStorage(visitedID, app.data.visited);
-}
-
-/**
  * Get the data from the API
  */
 function getPlaces() {
@@ -71,6 +41,9 @@ function getPlaces() {
     // Add the visited object to the API data
     app.data.visited = getFromStorage(visitedID);
 
+    // Add the filter object to the API data
+    app.data.filter = 'all';
+
     // Update the app data
     app.data.places = data;
   }).catch(function(error) {
@@ -80,13 +53,29 @@ function getPlaces() {
 }
 
 /**
+ * Get the hidden places based on the selected filter
+ * @param {Object} props The props object
+ * @param {Object} place The place object
+ */
+function getHidden(props, place) {
+  // If filter is "not visited" and place has been visited, hide it
+  if (props.filter === 'not-visited' && props.visited[place.id]) return 'hidden';
+
+  // If filter is "favorites" or "visited" and place has not been favorited or visited, hide it
+  if (props[props.filter] && !props[props.filter][place.id]) return 'hidden';
+
+  // Otherwise, show it
+  return '';
+}
+
+/**
  * Create HTML for the places
  * @param   {Object} props The places data
  * @returns {String}       The HTML
  */
 function getPlacesHTML(props) {
   return `${props.places.map(function(place) {
-    return `<article>
+    return `<article ${getHidden(props, place)}>
       <div>
         <img src="${place.img}" alt="">
       </div>
@@ -135,7 +124,51 @@ var app = new Reef('#app', {
   }
 });
 
+/**
+ * Handle change events
+ * @param {Event} event The event object
+ */
+function changeHandler(event) {
+  if (event.target.closest('.filters')) {
+    // Update the filter value
+    app.data.filter = event.target.value;
+  }
+}
+
+/**
+ * Handle click events
+ * @param {Object} event The event object
+ */
+function clickHandler(event) {
+  // If the event target has an attribute of data-object
+  if (event.target.closest('[data-object]')) {
+    // Get the value of data-object
+    var object = event.target.closest('[data-object]').getAttribute('data-object');
+
+    // Get the value of data-id
+    var id = event.target.closest('[data-id]').getAttribute('data-id');
+
+    // Toggle the place's favorites value
+    app.data[object][id] = app.data[object][id] ? false : true;
+  }
+}
+
+/**
+ * Handle render events
+ * @param {Event} event The event object
+ */
+function renderHandler(event) {
+  // Save the favorites object to localStorage
+  saveToStorage(favoritesID, app.data.favorites);
+
+  // Save the visited object to localStorage
+  saveToStorage(visitedID, app.data.visited);
+}
+
 getPlaces();
+
+// Listen for changes to the filters
+window.addEventListener('change', changeHandler);
 
 // Listen for clicks on buttons
 window.addEventListener('click', clickHandler);
